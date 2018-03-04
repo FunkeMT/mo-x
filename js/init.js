d7f7aa4cd80155d689435577e581192a8317d011
Markus Funke <funkemarkus@me.com>
2018-03-04 20:01:54 +0100

    impl header and technics section

--- js/init.js
+++ js/init.js
@@ -3,6 +3,19 @@
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
+    $('.scrollspy').scrollSpy({scrollOffset: 0});
+
+
+
+    var options = [
+      {selector: '#scrollfire-row-technics', offset: 400, callback: function(el) {
+        $(el).children().each(function (index) {
+          $(this).delay(1000 * index).animate({opacity: 1}, 1000);
+        })
+      } }
+    ];
+    Materialize.scrollFire(options);
+
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
