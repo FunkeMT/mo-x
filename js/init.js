93b58f24581a5ec610ab854522eac23bf251cbd6
Markus Funke <funkemarkus@me.com>
2018-03-08 18:36:17 +0100

    change scrollfire offset

--- js/init.js
+++ js/init.js
@@ -6,19 +6,19 @@
     $('.scrollspy').scrollSpy({scrollOffset: 0});
 
 
-    var options = [
+    var scrollfireOptions = [
       {selector: '#scrollfire-row-technics', offset: 400, callback: function(el) {
         $(el).children().each(function (index) {
           $(this).delay(1000 * index).animate({opacity: 1}, 1000);
         })
       } },
-      {selector: '#scrollfire-row-ux', offset: 700, callback: function(el) {
+      {selector: '#scrollfire-row-ux', offset: 400, callback: function(el) {
         $(el).children().each(function (index) {
           $(this).delay(1000 * index).animate({opacity: 1}, 1000);
         })
       } },
     ];
-    Materialize.scrollFire(options);
+    Materialize.scrollFire(scrollfireOptions);
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
